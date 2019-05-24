cac44cf2d392354930350fcebe4d5dbbb77a94bc
ronyshchenko <roma.onyshchenko@gmail.com>
2019-05-24 13:54:06 +0300

    my_commit

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
 'use strict';
+'use strict';
+let sorting = document.getElementById('sorting');
+
+sorting.onclick = function(e) {
+  if (e.target.tagName != 'TH') return;
+  sortGrid(e.target.cellIndex, e.target.getAttribute('data-type'));
+};
+
+function sortGrid(colNum, type) {
+  let tbody = sorting.getElementsByTagName('tbody')[0];
+  let rowsArray = [].slice.call(tbody.rows);
+  let compare;
+
+  switch (type) {
+    case 'number':
+      compare = function(rowA, rowB) {
+        return rowA.cells[colNum].innerHTML - rowB.cells[colNum].innerHTML;
+      };
+      break;
+    case 'string':
+      compare = function(rowA, rowB) {
+        return rowA.cells[colNum].innerHTML > rowB.cells[colNum].innerHTML;
+      };
+      break;
+  }
+
+  rowsArray.sort(compare);
+
+  sorting.removeChild(tbody);
+  
+  for (var i = 0; i < rowsArray.length; i++) {
+    tbody.appendChild(rowsArray[i]);
+  }
+
+  sorting.appendChild(tbody);
+}
